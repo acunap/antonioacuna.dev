3054c375d02350573d38a99fbc069562fa825ae7
acunap <hello@antonioacuna.dev>
2022-06-09 23:10:35 +0200

    Init project with Nuxt Content and create Navigation component

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script setup>
+const { data: navigation } = await useAsyncData('navigation', () => {
+  return fetchContentNavigation()
+})
+</script>
+
+<template>
+  <main>
+    <Navigation :navigation-tree="navigation"/>
+    <NuxtPage />
+  </main>
+</template>
---
+++ components/Navigation.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+    navigationTree: {
+        type: Array,
+        default: () => []
+    }
+})
+</script>
+
+<template>
+    <ul class="navigation">
+        <li v-for="item in navigationTree" :key="item.title">
+            <NuxtLink :to="item._path">
+                {{ item.title}}
+            </NuxtLink>
+            <Navigation v-if="item.children" :navigation-tree="item.children" class="sub-navigation" />
+        </li>
+    </ul>
+</template>
