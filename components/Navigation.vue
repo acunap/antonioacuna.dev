212ffd94840249d2f7afc7e7ad1ce730e82965f0
acunap <hello@antonioacuna.dev>
2022-06-11 09:54:46 +0200

    Add a new icon to close navigation menu on mobile devices

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -16,7 +16,8 @@ const toggleNavigationListVisibility = () => isNavigationListHidden.value = !isN
 <template>
     <nav class="relative flex flex-col justify-center items-end md:items-center">
         <button class="md:hidden" v-on:click="toggleNavigationListVisibility()">
-            <FontAwesomeIcon icon="fa-solid fa-bars" class="w-8"/>
+            <FontAwesomeIcon v-if="isNavigationListHidden" icon="fa-solid fa-bars" class="w-8"/>
+            <FontAwesomeIcon v-else icon="fa-solid fa-xmark"  class="w-8"/>
         </button>
         <NavigationList :navigation-tree="navigationTree" :class="{ hidden: isNavigationListHidden }" class="w-full h-screen top-full bg-white absolute md:h-auto md:static md:block md:bg-none" />
     </nav>
--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,11 +1,11 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faLinkedin, faGithubSquare } from '@fortawesome/free-brands-svg-icons'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
 
 config.autoAddCss = false
 
-library.add(faLinkedin, faGithubSquare, faBars)
+library.add(faLinkedin, faGithubSquare, faBars, faXmark)
 
 export default defineNuxtPlugin(nuxtApp => {
     nuxtApp.vueApp.component('FontAwesomeIcon', FontAwesomeIcon)
