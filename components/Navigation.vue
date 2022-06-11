432bc3fc18db2e2d77c1c7c54c530691c9bf1314
acunap <hello@antonioacuna.dev>
2022-06-11 13:08:16 +0200

    Add middleware to redirect root to /me

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,6 +1,4 @@
 <script setup>
-import { reactive } from 'vue'
-
 defineProps({
     navigationTree: {
         type: Array,
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,3 +1,13 @@
+<script setup>
+definePageMeta({
+    middleware: (to, from) => {
+        if (to.path == "/") {
+            return navigateTo("/me")
+        }
+    }
+})
+</script>
+
 <template>
   <article class="flex items-center overflow-y-auto py-16">
     <ContentDoc class="my-auto prose w-full max-w-none"/>
