361f776fb0fd8db9cc10525e22d87f31201247f7
acunap <hello@antonioacuna.dev>
2022-06-11 10:35:49 +0200

    Add collapse functionality to navbar

--- components/NavigationList.vue
+++ components/NavigationList.vue
@@ -3,17 +3,31 @@ defineProps({
     navigationTree: {
         type: Array,
         default: () => []
+    },
+    isChildrenList: {
+        type: Boolean,
+        default: () => false
     }
 })
+
+const showChildren = ref(false)
+
+const toggleShowChildren = () => showChildren.value = !showChildren.value
+const isCurrentRoute = (path) => useRoute().path == path
 </script>
 
 <template>
     <ul>
-        <li v-for="item in navigationTree" :key="item.title" class="text-center">
-            <NuxtLink :to="item._path">
+        <li v-for="item in navigationTree" :key="item.title" class="text-center font-bold" :class="{ 'font-normal': isChildrenList }" >
+            <button v-if="item.children" class="flex w-full space-x-4 justify-center items-center" v-on:click="toggleShowChildren()">
+                <span>{{ item.title }}</span>
+                <FontAwesomeIcon v-if="showChildren" icon="fa-solid fa-angle-up" class="w-3"/>
+                <FontAwesomeIcon v-else icon="fa-solid fa-angle-down" class="w-3" />
+            </button>
+            <NuxtLink v-else :to="item._path" :class="{ 'text-slate-500': isCurrentRoute(item._path) }">
                 {{ item.title}}
             </NuxtLink>
-            <NavigationList v-if="item.children" :navigation-tree="item.children" class="sub-navigation" />
+            <NavigationList v-if="item.children && showChildren" :navigation-tree="item.children" :isChildrenList="true"/>
         </li>
     </ul>
 </template>
--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,11 +1,11 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faLinkedin, faGithubSquare } from '@fortawesome/free-brands-svg-icons'
-import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faXmark, faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons'
 
 config.autoAddCss = false
 
-library.add(faLinkedin, faGithubSquare, faBars, faXmark)
+library.add(faLinkedin, faGithubSquare, faBars, faXmark, faAngleDown, faAngleUp)
 
 export default defineNuxtPlugin(nuxtApp => {
     nuxtApp.vueApp.component('FontAwesomeIcon', FontAwesomeIcon)
