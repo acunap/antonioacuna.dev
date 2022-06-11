8d435164111b0663df8d2b197abf5b559a007ae4
acunap <hello@antonioacuna.dev>
2022-06-11 12:29:10 +0200

    Duplicate button to fix bug with if/else

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -17,9 +17,11 @@ watch(useRoute(), () => isNavigationListHidden.value = true)
 
 <template>
     <nav class="relative flex flex-col justify-center items-end lg:items-center">
-        <button class="lg:hidden" v-on:click="toggleNavigationListVisibility()">
-            <FontAwesomeIcon v-if="isNavigationListHidden" icon="fa-solid fa-bars" class="w-8"/>
-            <FontAwesomeIcon v-else icon="fa-solid fa-xmark"  class="w-8"/>
+        <button v-if="isNavigationListHidden" class="lg:hidden" v-on:click="toggleNavigationListVisibility()">
+            <FontAwesomeIcon icon="fa-solid fa-bars" class="w-8"/>
+        </button>
+        <button v-else class="lg:hidden" v-on:click="toggleNavigationListVisibility()">
+            <FontAwesomeIcon icon="fa-solid fa-xmark" class="w-8"/>
         </button>
         <NavigationList :navigation-tree="navigationTree" :class="{ hidden: isNavigationListHidden }" class="w-full h-screen top-full bg-white absolute lg:h-auto lg:static lg:block lg:bg-none" />
     </nav>
